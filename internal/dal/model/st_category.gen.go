3e3a833edb9b3167cba2111595dd0a55688ef9d7
danny <dannyxuweb3@gmail.com>
2025-08-15 18:01:51 +0800

    update index

--- internal/dal/model/st_category.gen.go
+++ internal/dal/model/st_category.gen.go
@@ -15,15 +15,19 @@ type StCategory struct {
 	ID        int        `gorm:"column:id;type:INTEGER;primaryKey" json:"id"`
 	ParentID  int        `gorm:"column:parent_id;type:int(11);not null" json:"parent_id"`
 	Sort      int        `gorm:"column:sort;type:int(11);not null" json:"sort"`
+	Slug      string     `gorm:"column:slug;type:varchar(50);not null" json:"slug"`
 	Title     string     `gorm:"column:title;type:varchar(50);not null" json:"title"`
 	Icon      string     `gorm:"column:icon;type:varchar(255);not null" json:"icon"`
 	IconCss   string     `gorm:"column:icon_css;type:varchar(255);not null" json:"icon_css"`
 	Desc      string     `gorm:"column:desc;type:varchar(255);not null" json:"desc"`
 	Level     int32      `gorm:"column:level;type:integer;not null" json:"level"`
-	IsUsed    bool       `gorm:"column:is_used;type:bool;default:false" json:"is_used"`
+	IsUsed    bool       `gorm:"column:is_used;type:bool;default:false" json:"is_used"` //@deprecated
+	Status    int        `gorm:"column:status;type:tinyint;default:0;not null" json:"status"`
 	CreatedAt *time.Time `gorm:"column:created_at;type:datetime;not null;default:CURRENT_TIMESTAMP not null" json:"created_at"`
 	UpdatedAt *time.Time `gorm:"column:updated_at;type:datetime;not null;default:CURRENT_TIMESTAMP not null" json:"updated_at"`
 	DeletedAt *time.Time `gorm:"column:deleted_at;type:datetime" json:"deleted_at"`
+	Count     int        `gorm:"column:count;type:int;default:0;not null" json:"count"`
+	FreeCount int        `gorm:"column:free_count;type:int;default:0;not null" json:"free_count"`
 }
 
 // TableName StCategory's table name
